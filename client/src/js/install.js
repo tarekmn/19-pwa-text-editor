1bad9f271defe97b897e1c5107c9105ff010c066
tarekmn <nytarek@gmail.com>
2022-11-10 14:54:05 -0600

    issues initalizing

---
+++ client/src/js/install.js
@@ -0,0 +1,28 @@
+const butInstall = document.getElementById('buttonInstall');
+
+
+window.addEventListener('beforeinstallprompt', (event) => {
+  window.deferredPrompt = event;
+  butInstall.classList.toggle('hidden', false);
+});
+
+// This listener fires when the user clicks the "install" button.
+butInstall.addEventListener('click', async () => {
+  const promptEvent = window.deferredPrompt;
+
+  if (!promptEvent) {
+    return;
+  }
+
+  promptEvent.prompt();
+
+
+  window.deferredPrompt = null;
+  butInstall.classList.toggle('hidden', false);
+
+
+});
+
+window.addEventListener('appinstalled', (event) => {
+  window.deferredPrompt = null;
+});
